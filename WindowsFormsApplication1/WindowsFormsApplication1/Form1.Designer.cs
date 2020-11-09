b35d2fb5f31eeafec2740dbc4c5a643fbe68f279
郑星龙 <telikers@hotmail.com>
2020-11-09 18:59:59 +0800

    完善了程序重启退出的问题，并改善了显示结果的效果

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -29,50 +29,43 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(112, 123);
+            this.textBox1.Location = new System.Drawing.Point(121, 76);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 25);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(56, 198);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(343, 94);
-            this.listBox1.TabIndex = 3;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(121, 169);
+            this.button2.Location = new System.Drawing.Point(130, 142);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(283, 169);
+            this.button3.Location = new System.Drawing.Point(283, 142);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 5;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(283, 122);
+            this.button1.Location = new System.Drawing.Point(283, 76);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
@@ -80,15 +73,27 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox2
+            // 
+            this.textBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox2.Location = new System.Drawing.Point(27, 218);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(400, 50);
+            this.textBox2.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(459, 294);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -100,10 +105,10 @@
 
         #endregion
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -14,43 +14,43 @@ namespace WindowsFormsApplication1
     public partial class Form1 : Form
     {
         int i;
-        Random r = new Random();
         int n;
         public Form1()
         {
            
             InitializeComponent();
-            n= r.Next(100);
+            Random r = new Random();
+            n = r.Next(100);
+            Console.WriteLine(n);
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-        
-        }
-
-      
-            
-           
+        }         
 private void button1_Click(object sender, EventArgs e)
         {
-            
-                
+            String str;  
                 if (i > n)
                 {
-                    MessageBox.Show("大了");
-                }
+                str = "大了:" + i;
+                MessageBox.Show(str);
+                this.textBox2.Text = this.textBox2.Text + str + "\r\n";
+            }
                 else if (i < n)
                 {
-                    MessageBox.Show("小了");
-            }else {
-                MessageBox.Show("对了，数字为:" + n);
-                this.textBox1.Text = "";
-                this.listBox1.SelectedValue = "" + n;
+                str = "小了:" + i;
+                MessageBox.Show(str);
+                this.textBox2.Text = this.textBox2.Text + str + "\r\n";
+              
             }
-            
-            
-
+            else {  
+                this.textBox1.Text = "";
+                str = "对了，数字为:" + n;
+                MessageBox.Show(str);
+                this.textBox2.Text = this.textBox2.Text + str + "\r\n";
+                return;
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -67,5 +67,17 @@ private void button1_Click(object sender, EventArgs e)
             
             
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          Application.Restart();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+       
     }
 }
